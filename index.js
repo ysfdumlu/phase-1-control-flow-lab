a4a1a4ae26fb7e76abc679b0e912bc16140f673e
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-02-04 09:57:32 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(x){
   // Write your code here!
+   let price
+ if(x<=400) price ="This one is on me!"
+ else if (x>400 && x<=2000) price = "That will be twenty bucks."
+ else if (x>2000 && x<=2500) price = "I will gladly take your thirty bucks."
+ else if (x>2500) price = "No can do."
+ return price
 }
+console.log(scuberGreetingForFeet(500))
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+    if(city==="NYC") return "Ok, sounds good."
+  else return "No go."
 }
+console.log(ternaryCheckCity("NYC"))
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
+  if(tip==="generous") return "Thank you so much."
+  else if(tip==="not as generous") return "Thank you."
+  else return "Bye."
   // Write your code here!
 }
+console.log(switchOnCharmFromTip("not as genorous"))
